22e056086d73f93325c1ca949ec523e2e4b46577
Oleg Rubanov <165693738+orubanov@users.noreply.github.com>
2024-04-03 00:07:21 +0300

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,7 @@
+stroka = 'это моя строка'
+print(stroka[0])
+print(stroka[-1])
+print(stroka[2:5]) #противоречиво сформулированно задание
+print(stroka[::-1])
+print(len(stroka))
+print(stroka+stroka)
